6d50efd9e3761e5958da60b6c4563cbf0827d50f
ChiiKang <tangchiikang@gmail.com>
2025-04-08 01:14:41 +0800

    Final Edit

--- backend/app.py
+++ backend/app.py
@@ -13,7 +13,7 @@ app = FastAPI()
 # Configure CORS for frontend access - make sure this is before any routes
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://scam-detek.vercel.app/"],  # Specifically allow your React frontend
+    allow_origins=["http://3.107.236.104:8000"],  # Specifically allow your React frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
